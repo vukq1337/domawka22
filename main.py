07e050207e5e74e7a93dce9f0dcc2e196e738723
Evgeny Shtykhno <denisiksht.k9@gmail.com>
2023-11-04 13:21:38 +0200

    Add file

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+print("Привіт світ")
+
+a = 10
+b = 5
+c = 3
+
+сума = a + b + c
+різниця = a - b - c
+
+print(f"Сума: {сума}, Різниця: {різниця}")
 
 
+for i in range(15):
+    print("привіт світ")
 
-print('Hello World')
+number = int(input("Введіть число: "))
+if number % 2 == 0:
+    print("Введене число парне.")
+else:
+    print("Введене число непарне.")
